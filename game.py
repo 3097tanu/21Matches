6143075b2e6ad57a04228a7746dfdba2342d0fae
Tanushree Agarwal <3097tanu@gmail.com>
2024-05-25 03:46:46 -0400

    Add multiplayer game mode

---
+++ game.py
@@ -0,0 +1,58 @@
+import random
+
+#multiplayer or play with computer
+#game_mode = input("Play with a friend (press 'F') or Play with Computer (press 'C'): ").upper()
+
+def multiplayer_game():
+    player1_name = input("Enter 1st player's name: ")
+    player2_name = input("Enter 2nd player's name: ")
+    players = [player1_name, player2_name]
+
+    turn = random.choice(range(len(players)))
+    print(f"\n{players[turn]} will play first!\n")
+
+        
+    total_matches_picked = 0
+    matches_left = 21
+    print(f"\nTotal matches picked: {total_matches_picked}/21.")
+    print(f"Total Matches left: {matches_left}.\n")
+
+    while total_matches_picked < 21:
+        while True: #check for correct pick
+            turn_matches_picked = int(input(f"{players[turn]}, pick matches between 1 and 3: "))
+            if matches_left >=3:
+                if turn_matches_picked < 1 or turn_matches_picked > 3:
+                    print(f"Invalid input! You can only pick 1, 2 or 3 matches.\nTry again!\n")
+                else:
+                    break
+            else:
+                if turn_matches_picked < 1 or turn_matches_picked > matches_left:
+                    print(f"Invalid input! You have only {matches_left} matches left to pick.\nTry again!\n")
+                else:
+                    break
+            
+        total_matches_picked = total_matches_picked + turn_matches_picked
+        matches_left = matches_left - turn_matches_picked
+        
+        print(f"\nTotal matches picked: {total_matches_picked}/21.")
+        print(f"Total Matches left: {matches_left}.\n")
+
+        #turn = player2_name if turn == player1_name else player1_name
+        turn = (turn+1)%len(players)
+
+    print(f"{players[turn]} is the winner!")
+
+# match game_mode:
+#   #play with a friend
+#   case "P": 
+print("\nYou'll play with a friend.\n")
+multiplayer_game()
+
+
+
+      
+#play with computer
+#case "C":
+    #player wins toss
+    #computer wins toss
+
